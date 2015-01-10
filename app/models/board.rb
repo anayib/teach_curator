81d53b7ea4c8064290eecfcab5f448598310db5f
anayib <nayib.abdala@gmail.com>
2015-01-10 14:19:56 -0500

    Add enum for content_format and call the collection in the form to pick the format of the lesson

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,5 @@
 class Board < ActiveRecord::Base
+
   belongs_to :user
   has_many :lessons, :dependent => :destroy
   accepts_nested_attributes_for :lessons, reject_if: lambda {|attributes| attributes['title'].blank?}, allow_destroy: true
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,5 @@
 class Lesson < ActiveRecord::Base
+  enum content_format: [:image, :video, :url, :text]
   belongs_to :board
+
 end
