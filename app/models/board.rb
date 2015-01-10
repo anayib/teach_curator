5b4308422456c9754a08d642c0ca921e7eab9da4
anayib <nayib.abdala@gmail.com>
2015-01-10 10:39:14 -0500

    Add accepts nested attributes from lesson model into board model

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,5 @@
 class Board < ActiveRecord::Base
   belongs_to :user
   has_many :lessons
+  accepts_nested_attributes_for :lessons, reject_if: lambda {|attributes| attributes['content'].blank?}, allow_destroy: true
 end
