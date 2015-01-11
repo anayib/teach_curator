f90ac7edc29ec39249cddb5456895cc05084df4d
Ricardo Rojas <reddrum7@gmail.com>
2015-01-11 10:09:28 -0500

    Add validation inside user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  
+
   has_many :boards, :dependent => :destroy
+
+  validates :name, :last_name, :username, presence: true 
+  validates :email, :username, uniqueness: true
+  
 end
